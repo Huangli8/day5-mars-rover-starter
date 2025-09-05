78ef049f1203389bdbc079534cb1ab99755f4fae
HUANGLI8 <lily.huang@oocl.com>
2025-09-05 16:03:42 +0800

    test:add some move command test

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -6,13 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
     @Test
-    void should_move_when_executeCommand_given_M() {
+    void should_move_when_executeCommand_given_M_origin_direction_N() {
         //given
         Location location = new Location(0, 0, Direction.N);
         MarsRover marsRover = new MarsRover(location);
         //when
         marsRover.executeCommand(Command.M);
         Location currentLocation = new Location(0,1,Direction.N);
+        //then
+        assertEquals(currentLocation.getXCoordinate(), marsRover.getLocation().getXCoordinate());
+        assertEquals(currentLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+    @Test
+    void should_move_when_executeCommand_given_M_origin_direction_S() {
+        //given
+        Location location = new Location(0, 0, Direction.S);
+        MarsRover marsRover = new MarsRover(location);
+        //when
+        marsRover.executeCommand(Command.M);
+        Location currentLocation = new Location(0,-1,Direction.S);
+        //then
+        assertEquals(currentLocation.getXCoordinate(), marsRover.getLocation().getXCoordinate());
+        assertEquals(currentLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+    @Test
+    void should_move_when_executeCommand_given_M_origin_direction_W() {
+        //given
+        Location location = new Location(0, 0, Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+        //when
+        marsRover.executeCommand(Command.M);
+        Location currentLocation = new Location(-1,0,Direction.W);
+        //then
+        assertEquals(currentLocation.getXCoordinate(), marsRover.getLocation().getXCoordinate());
+        assertEquals(currentLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
+        assertEquals(currentLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
+    @Test
+    void should_move_when_executeCommand_given_M_origin_direction_E() {
+        //given
+        Location location = new Location(0, 0, Direction.E);
+        MarsRover marsRover = new MarsRover(location);
+        //when
+        marsRover.executeCommand(Command.M);
+        Location currentLocation = new Location(1,0,Direction.E);
         //then
         assertEquals(currentLocation.getXCoordinate(), marsRover.getLocation().getXCoordinate());
         assertEquals(currentLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
